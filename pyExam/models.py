4fdefb172cb95ce609aee7988094afd7a5d31c59
dylandalton <dylandalton.za@gmail.com>
2020-04-07 16:51:04 +0100

    initial commit

--- pyExam/models.py
+++ pyExam/models.py
@@ -1,3 +1,18 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    heading_text = models.CharField(max_length=40)
+    date_and_time = models.DateTimeField('date published')
+    story_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.heading_text
+    def was_published_recently(self):
+        return self.date_and_time >= timezone.now() - datetime.timedelta(days=1)
+
+class New_Article(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.user_text
--- pyExam/views.py
+++ pyExam/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_article_list = Article.objects.order_by('-date_and_time')[:5]
+    template = loader.get_template('pyExam/index.html')
+    context = {
+        'latest_article_list' : latest_article_list
+    }
+    return HttpResponse(template.render(context, request))
+
+#def detail(request, article_id):
+    #return HttpResponse("You're looking at article %s." % article_id)
+
+#def results(response, article_id):
+    #response = "You're looking at the results of article %s"
+     #return HttpResponse(response % article_id)
